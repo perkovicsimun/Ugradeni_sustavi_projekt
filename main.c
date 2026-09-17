5cee7b1ec9fb825f56916b48313a2b07c4ab3a05
perkovicsimun <perkovic.simun05@gmail.com>
2026-09-17 02:47:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,58 +8,63 @@
 #include "zaslon.h"
 #include "pumpa.h"
 
-int main(void){
-
+int main(void)
+{
     vaga_init();
-    pumpa_init();
     zaslon_init();
     temperatura_init();
     uart_init();
+    pumpa_init();
+
+    _delay_ms(500);
     vaga_tare();
 
-    while(1){
+    while(1)
+    {
         float masa_vage = vaga_read_grams();
         float temp_zraka = temperatura_read();
-        int32_t postotak = (int32_t)((masa_vage * 100.0) / 180.0);
 
-        if(postotak < 0){
+        int32_t postotak =
+            (int32_t)((masa_vage * 100.0) / 150.0);
+
+        if(postotak < 0)
             postotak = 0;
-        }
-        if(postotak >=100){
+
+        if(postotak >= 100)
             postotak = 100;
-        }
 
         zaslon_set_cursor(0, 0);
         zaslon_print("Voda: ");
         zaslon_printint(postotak);
-        zaslon_print("%");
+        zaslon_print("%   ");
 
         zaslon_set_cursor(1, 0);
         zaslon_print("Temp: ");
         zaslon_printint((int32_t)temp_zraka);
         zaslon_print(" C   ");
 
-        if(postotak<20){
+        uart_printstr("Voda: ");
+        uart_printint(postotak);
+        uart_printstr("% | Temp: ");
+        uart_printint((int32_t)temp_zraka);
+        uart_println(" C");
+
+
+        if(postotak < 20)
+        {
             pumpa_on();
         }
-        else if(postotak>=80){
-            pumpa_off();
-        }
+        if(postotak < 50)
+{
+    pumpa_on();
+    _delay_ms(500);
+    pumpa_off();
 
-        _delay_ms(100);
+    _delay_ms(250);
+}
 
+        _delay_ms(500);
     }
 
-
-
-
-
-
-
-
-
-
-    
     return 0;
-
 }
